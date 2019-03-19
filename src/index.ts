0d7c7658a11e8a157850f54bdc60050cf66d81ed
Borre <borremosch@gmail.com>
2019-03-19 22:00:22 +0100

    Removed debugging statement

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,6 @@ const args = parseArgs(process.argv);
 validateArgs(args);
 const inputs = getInputDataFromArgs(args);
 
-console.log(JSON.stringify(inputs[0].data));
-
 // Generate output
 const output = mergeInputs(inputs);
 
