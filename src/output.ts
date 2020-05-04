9bc52c4bb80a373dd56129eec6d98dd4cc478307
John Ruble <john.ruble@atomicobject.com>
2020-05-04 16:59:56 -0400

    use toXml sanitize option

--- src/output.ts
+++ src/output.ts
@@ -6,7 +6,7 @@ import { toXml } from 'xml2json';
 const XML_HEADER = '<?xml version="1.0" ?>\n';
 
 export function writeOutput(outputFile: string, output: CoberturaJson) {
-  const outputXml = XML_HEADER + addSelfClosingTags(toXml(JSON.stringify(output)));
+  const outputXml = XML_HEADER + addSelfClosingTags(toXml(JSON.stringify(output), { sanitize: true }));
   const outputFilename: string = outputFile;
 
   fs.writeFileSync(outputFilename, outputXml);
